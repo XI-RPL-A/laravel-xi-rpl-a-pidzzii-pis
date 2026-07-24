ef2c72c757e8a761da2009877abc47afdc031952
Hafidzy <fidzy.mohamed@gmail.com>
2026-07-24 14:21:11 +0700

    Praktikum Route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>NIS : 25996/2197.063</h3><br>
+    <h3>NAMA : HAFIDZY</h3><br>
+    <h3>KELAS : XI-RPLA</h3>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,47 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProgramController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function() {
+//     return 'Selamat Datang Di Website Dengan Laravel';
+// });
+// Route::get('/about', function(){
+//     return 'NIS : 25996/2197.063
+//     NAMA : HAFIDZY
+//     KELAS : XI-RPLA';
+// });
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Ini adalah halaman Artikel dengan ID : '. $id;
+// });
+
+// Route::get('/', [HomeController::class, 'index']);
+
+// Route::get('/about', [AboutController::class, 'about']);
+
+// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::get('home', [HomeController::class, 'index']);
+
+Route::prefix('')->group(function(){
+    Route::get('/product', [ProductController::class, 'produk']);
 });
+ 
+Route::prefix('')->group(function(){
+    Route::get('/program', [ProgramController::class, 'program']);
+});
+
+
+Route::get('/news/{id}', function ($id) {
+    return 'Ini adalah halaman News dengan ID : '. $id;
+});
+Route::get('/about', function(){
+   return 'NIS : 25996/2197.063
+    NAMA : HAFIDZY
+    KELAS : XI-RPLA';
+}); 
